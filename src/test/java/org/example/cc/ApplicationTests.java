860ed4e2da63970eb6ff3e57c84568057087450f
pjmoo <00pjmoooo@gmail.com>
2026-09-15 14:32:18 +0900

    Initial commit

---
+++ src/test/java/org/example/cc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
